575a211c20db25ebabef860a968d6098c7672b32
Devik Reddy <bdrwork97@gmail.com>
2026-03-19 14:44:13 -0500

    created s3 bucket

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 provider "aws"{
-    region = "us-east-1"
+    region = var.region
 
 }
 
 resource "aws_s3_bucket" "my-devikbucket"{
-    bucket = "my-deviks3-bucket"
+    bucket = var.bucket_name
 }
--- varibles.tf
+++ varibles.tf
@@ -1,7 +1,13 @@
 variable "region" {
-    description = "AWS region creating"
-    type = string
-    default = "us-east-2"
-
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
 }
+
+variable "bucket_name" {
+  description = "Name of the S3 bucket"
+  type        = string
+  default     = "devik-practice-bucket-8823"
+}
+
 
