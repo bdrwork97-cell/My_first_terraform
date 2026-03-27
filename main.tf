24a0eaad62e7c7e3d9815fd000103378e48c869a
Devik Reddy <bdrwork97@gmail.com>
2026-03-27 17:04:02 -0500

    added more backupdetails

--- main.tf
+++ main.tf
@@ -9,3 +9,12 @@ resource "aws_s3_bucket" "my-devikbucket"{
 resource "aws_backup_vault" "devik-practice-vault" {
     name = "devik_cloud_vault"
 }
+resource "aws_backup_plan" "devik_new_plan" {
+  name = "devik_new_plan"
+
+  rule {
+    rule_name         = "devik_backup_rule"
+    target_vault_name = aws_backup_vault.devik-practice-vault.name
+    schedule          = "cron(0 12 * * ? *)"
+  }
+}
