46e9a843879f8ea07de3cf63d645c189bdac67e9
Devik Reddy <bdrwork97@gmail.com>
2026-03-27 17:12:58 -0500

    removed the resources

--- main.tf
+++ main.tf
@@ -6,15 +6,3 @@ provider "aws"{
 resource "aws_s3_bucket" "my-devikbucket"{
     bucket = var.bucket_name
 }
-resource "aws_backup_vault" "devik-practice-vault" {
-    name = "devik_cloud_vault"
-}
-resource "aws_backup_plan" "devik_new_plan" {
-  name = "devik_new_plan"
-
-  rule {
-    rule_name         = "devik_backup_rule"
-    target_vault_name = aws_backup_vault.devik-practice-vault.name
-    schedule          = "cron(0 12 * * ? *)"
-  }
-}
